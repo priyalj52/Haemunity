821923752f585f7b9ccf3859063afb91196af1e4
priyal <priyaljaiin52@gmail.com>
2024-05-07 23:03:36 +0530

    logout fixed

--- src/components/shared/Layout/Header.js
+++ src/components/shared/Layout/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BiDonateBlood, BiUserCircle } from "react-icons/bi";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -11,10 +11,10 @@ const Header = () => {
   // logout handler
   const handleLogout = () => {
     localStorage.clear();
-    
+
     // alert("Logout Successfully");
-  
-    toast.success(' Logout Successfully!', {
+
+    toast.success(" Logout Successfully!", {
       position: "top-right",
       autoClose: 2000,
       hideProgressBar: false,
@@ -23,41 +23,34 @@ const Header = () => {
       draggable: true,
       progress: undefined,
       theme: "dark",
-    
-      });
+    });
     navigate("/login");
   };
-const askAgain=()=>{
-  let answer = window.prompt(
-    "Are You Sure you Want To logout",
-    "Sure"
-  );
-  if (!answer) return;
- handleLogout()
- 
-}
+  const askAgain = () => {
+    let answer = window.prompt("Are You Sure you Want To logout", "Sure");
+    if (!answer) return;
+    handleLogout();
+  };
 
   return (
     <>
       <nav className="navbar">
-    
         <div className="container-fluid ">
           <div className="navbar-brand h1 ">
             <BiDonateBlood color="#2d3282" /> Haemunity
           </div>
           <ToastContainer
-position="top-right"
-autoClose={2000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-
-/>
+            position="top-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
           <ul className="navbar-nav flex-row">
             <li className="nav-item mx-3">
               <p className="nav-link">
@@ -77,14 +70,19 @@ theme="dark"
               </li>
             ) : (
               <li className="nav-item mx-3">
-                {user?.role!=='donar' && <Link to="/" className="nav-link">
-                  Home
-                </Link>}
+                {user?.role !== "donar" && (
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                )}
               </li>
             )}
             <li className="nav-item mx-3">
-          
-              <button style={{backgroundColor: '#2d3282',color:'white'}} className="btn" onClick={askAgain}>
+              <button
+                style={{ backgroundColor: "#2d3282", color: "white" }}
+                className="btn"
+                onClick={askAgain}
+              >
                 Logout
               </button>
             </li>
