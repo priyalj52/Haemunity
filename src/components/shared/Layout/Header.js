2aeb43afd6504f141f78db90cb6ea83bc60ae6cc
priyal <priyaljaiin52@gmail.com>
2024-05-07 23:00:15 +0530

    alert removed

--- src/components/shared/Layout/Header.js
+++ src/components/shared/Layout/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BiDonateBlood, BiUserCircle } from "react-icons/bi";
 import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Header = () => {
   const { user } = useSelector((state) => state.auth);
   const navigate = useNavigate();
@@ -9,17 +11,53 @@ const Header = () => {
   // logout handler
   const handleLogout = () => {
     localStorage.clear();
-    alert("Logout Successfully");
+    
+    // alert("Logout Successfully");
+  
+    toast.success(' Logout Successfully!', {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    
+      });
     navigate("/login");
   };
+const askAgain=()=>{
+  let answer = window.prompt(
+    "Are You Sure you Want To logout",
+    "Sure"
+  );
+  if (!answer) return;
+ handleLogout()
+ 
+}
 
   return (
     <>
       <nav className="navbar">
+    
         <div className="container-fluid ">
           <div className="navbar-brand h1 ">
             <BiDonateBlood color="red" /> Blood Bank App
           </div>
+          <ToastContainer
+position="top-right"
+autoClose={2000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="dark"
+
+/>
           <ul className="navbar-nav flex-row">
             <li className="nav-item mx-3">
               <p className="nav-link">
@@ -45,7 +83,7 @@ const Header = () => {
               </li>
             )}
             <li className="nav-item mx-3">
-              <button className="btn btn-danger" onClick={handleLogout}>
+              <button className="btn btn-danger" onClick={askAgain}>
                 Logout
               </button>
             </li>
